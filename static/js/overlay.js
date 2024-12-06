82e06d67ef331c862b833bbc252e5ddec5bf2e9b
OssianGH <poroscarin@gmail.com>
2024-12-06 10:43:11 -0500

    overlay animation

---
+++ static/js/overlay.js
@@ -0,0 +1,21 @@
+// Wait for DOM content to load
+document.addEventListener('DOMContentLoaded', () => {
+  // Add click event listener to the Add exercise button
+  document.getElementById('add-exercise').addEventListener('click', showOverlay);
+
+  // Add click event listener to the Show overlay button
+  document.getElementById('hide-overlay').addEventListener('click', hideOverlay);
+});
+
+function showOverlay() {
+  // Remove hiding class if it exists
+  overlay.classList.remove('hiding');
+
+  // Add active class to the overlay div
+  document.getElementById('overlay').classList.add('active');
+}
+
+function hideOverlay() {
+  // Remove active class to the overlay div
+  document.getElementById('overlay').classList.remove('active');
+}
