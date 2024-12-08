ca10e97361ddbd257c939f49cf1d2678b519ae6a
OssianGH <poroscarin@gmail.com>
2024-12-07 20:03:11 -0500

    add and remove exercise

--- static/js/addExercise.js
+++ static/js/addExercise.js
@@ -1,62 +1,75 @@
 async function addExercise(exerciseId, event) {
+  // Prevent the default anchor tag behavior
   event.preventDefault();
 
-  // Remove active class to the overlay div
+  // Hide overlay
   document.getElementById('overlay').classList.remove('active');
 
   try {
+    // Fetch the exercise data
     const response = await fetch(`/exercise/${exerciseId}`);
 
+    // Check if the response is not OK
     if (!response.ok) {
       throw new Error('Exercise not found');
     }
 
+    // Parse the JSON response
     const exercise = await response.json();
 
-    // Replace spaces with underscores in the exercise name
-    name_underscore = exercise.name.toLowerCase().replace(/ /g, "_");
+    // Get the exercises container
+    const exercisesContainer = document.getElementById("exercises");
 
-    // Create a new exercise element
+    // Get the number of exercises in the container
+    const exerciseNumber = exercisesContainer.childElementCount + 1;
+
+    // Set the exercise string
+    const exerciseNumberString = `exercise-${exerciseNumber}`;
+
+    // Replace spaces with underscores in the exercise name
+    const nameUnderscore = exercise.name.toLowerCase().replace(/ /g, "_");
+
+    // Create the new exercise div
     const newExercise = document.createElement("div");
 
     // Assign a unique ID to the new exercise
-    newExercise.id = `exercise-${exercise.id}`;
+    newExercise.setAttribute('id', exerciseNumberString)
 
-    // Assign the class name to the new exercise
-    newExercise.className = "exercise";
+    // Assign the class to the new exercise
+    newExercise.setAttribute('class', 'exercise')
 
     // Set the inner HTML of the new exercise
     newExercise.innerHTML = `
-    <div class="exercise-name flex center-align between-justify">
+    <input type="hidden" name="${exerciseNumberString}-id" value="${exercise.id}">
+    <div class="exercise-name flex gap center-align between-justify">
       <div class="image-container">
-        <img class="image" src="/static/images/exercises/${exercise.muscle_group.toLowerCase()}/${name_underscore}.png" alt="${exercise.name}">
+        <img class="image" src="/static/images/exercises/${exercise.muscle_group.toLowerCase()}/${nameUnderscore}.png" alt="${exercise.name}">
       </div>
       <h3 class="h3 text-center no-margin">${exercise.name}</h3>
-      <button id="add-set" class="btn" type="button">Add set</button>
+      <button class="btn" type="button" onclick="addSet(${exerciseNumber})">Add set</button>
     </div>
-    <div id="set-1" class="exercise-set flex gap center-align">
-      <p class="no-margin">1</p>
-      <div class="input-wrapper flex gap center-align evenly-justify">
-        <div class="input-group">
-          <input class="input" autocomplete="off" name="weight" type="text">
-          <label class="input-label">Weight</label>
+    <div id="${exerciseNumberString}-sets">
+      <div id="${exerciseNumberString}-set-1" class="exercise-set flex gap center-align">
+        <p class="no-margin">1</p>
+        <div class="input-wrapper flex gap center-align evenly-justify">
+          <div class="input-group">
+            <input class="input" autocomplete="off" name="${exerciseNumberString}-set-1-weight" type="text">
+            <label class="input-label">Weight</label>
+          </div>
+          <div class="input-group">
+            <input class="input" autocomplete="off" name="${exerciseNumberString}-set-1-reps" type="text">
+            <label class="input-label">Reps</label>
+          </div>
         </div>
-        <div class="input-group">
-          <input class="input" autocomplete="off" name="reps" type="text">
-          <label class="input-label">Reps</label>
-        </div>
+        <button class="square-btn" type="button" onclick="removeSet(${exerciseNumber}, 1)"><i class="fa fa-x"></i></button>
       </div>
     </div>`;
 
-    // newExercise.querySelector("#add-set").addEventListener("click", addSet);
-
-    const exercisesContainer = document.getElementById("exercises");
-
     // Append the new exercise to the exercises container
     exercisesContainer.appendChild(newExercise);
+
+    // Bind the animation for the new inputs to display animation 
     window.bindInputListeners();
-
-
   } catch (error) {
     console.error('Error:', error);
     alert('Failed to add exercise.');
---
+++ static/js/removeExercise.js
@@ -0,0 +1,43 @@
+function removeExercise(exerciseCount) {
+  // Set the exercise string
+  const currentExercise = `exercise-${exerciseCount}`;
+
+  // Get the exercise to remove
+  const exercise = document.getElementById(currentExercise);
+
+  // Check if the exercise exists
+  if (exercise) {
+    exercise.remove();
+  }
+
+  // Re-index exercises
+  reindexExercises();
+}
+
+
+function reindexExercises() {
+  // Set the exercise string
+  const exercisesContainer = document.getElementById('exercises');
+
+  Array.from(exercisesContainer.children).forEach((exercise, index) => {
+    const oldExerciseNumberString = exercise.getAttribute('id');
+
+    const newExerciseNumber = index + 1;
+    const newExerciseNumberString = `exercise-${newExerciseNumber}`;
+
+    // Update ID of the exercise
+    exercise.setAttribute('id', newExerciseNumberString);
+
+    // Update hidden input for exercise ID
+    exercise.querySelector(`input[name="${oldExerciseNumberString}-id"]`).setAttribute('name', `${newExerciseNumberString}-id`)
+
+    // Update the Add set button
+    exercise.querySelector('.btn').setAttribute('onclick', `addSet('${newExerciseNumber}')`);
+
+    // Update the sets container
+    exercise.querySelector(`#${oldExerciseNumberString}-sets`).setAttribute('id', `${newExerciseNumberString}-sets`);;
+
+    // Re-index sets within the exercise
+    reindexSets(newExerciseNumber);
+  });
+}
